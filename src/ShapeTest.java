e9d2c3c283b8e7ccbbe4f16c2c2982c3c0b8b5b3
MainKernel <86931698+MainKernel@users.noreply.github.com>
2022-09-24 06:20:30 +0300

    ShapeTest update, Git .ignore update

--- src/ShapeTest.java
+++ src/ShapeTest.java
@@ -1,15 +1,15 @@
 public class ShapeTest {
     public static void main(String[] args) {
 
-        Circle Circle = new Circle(12, 2, 19);
-        Square Square = new Square(8);
-        EquilateralTriangle tri = new EquilateralTriangle(12,5,7);
-        Rectangle rec = new Rectangle(18, 15);
-        Rhombus rom = new Rhombus(32);
+        Shape circle = new Circle(12, 2, 19);
+        Shape square = new Square(8);
+        Shape triangle = new EquilateralTriangle(12,5,7);
+        Shape rec = new Rectangle(18, 15);
+        Shape rom = new Rhombus(32);
 
-        new ShapePrint(Circle);
-        new ShapePrint(Square);
-        new ShapePrint(tri);
+        new ShapePrint(circle);
+        new ShapePrint(square);
+        new ShapePrint(triangle);
         new ShapePrint(rec);
         new ShapePrint(rom);
     }
